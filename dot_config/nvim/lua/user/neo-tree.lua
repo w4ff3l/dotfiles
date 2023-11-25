37087c2d67563d23bdb779ec1c651fc925fbaba5
W4ff3l <w4ff3l@gmail.com>
2023-11-25 10:22:15 +0100

    reconfigure neo-tree to reveal

--- dot_config/nvim/lua/user/neo-tree.lua
+++ dot_config/nvim/lua/user/neo-tree.lua
@@ -138,26 +138,8 @@ neo_tree.setup({
     filesystem = {
         filtered_items = {
             visible = false, -- when true, they will just be displayed differently than normal items
-            hide_dotfiles = true,
-            hide_gitignored = true,
-            hide_hidden = true, -- only works on Windows for hidden files/directories
-            hide_by_name = {
-                --'node_modules'
-            },
-            hide_by_pattern = { -- uses glob style patterns
-                --'*.meta',
-                --'*/src/*/tsconfig.json',
-            },
-            always_show = { -- remains visible even if other settings would normally hide it
-                --'.gitignored',
-            },
-            never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
-                --'.DS_Store',
-                --'thumbs.db'
-            },
-            never_show_by_pattern = { -- uses glob style patterns
-                --'.null-ls_*',
-            },
+            hide_dotfiles = false,
+            hide_gitignored = false,
         },
         follow_current_file = {
             enabled = false,                    -- This will find and focus the file in the active buffer every time
