bcc668285e0d91529a7f807b26871aa01bf9543a
W4ff3l <w4ff3l@gmail.com>
2023-11-25 08:55:34 +0100

    disable lsp highlighting

--- dot_config/nvim/lua/user/lsp/lspconfig.lua
+++ dot_config/nvim/lua/user/lsp/lspconfig.lua
@@ -7,6 +7,10 @@ vim.api.nvim_create_autocmd('LspAttach', {
         -- Enable completion triggered by <c-x><c-o>
         vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
+        -- Disable lsp highlighting
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        client.server_capabilities.semanticTokensProvider = nil
+
         -- Buffer local mappings.
         -- See `:help vim.lsp.*` for documentation on any of the below functions
         local opts = { buffer = ev.buf }
