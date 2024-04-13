53fbd58c686961df1b9d5b3f10852e78eb9d96c6
W4ff3l <w4ff3l@gmail.com>
2024-04-13 14:06:19 +0200

    xml: add xml lsp config

---
+++ dot_config/nvim/lua/user/languages/xml.lua
@@ -0,0 +1,8 @@
+local status_ok, lspconfig = pcall(require, 'lspconfig')
+if not status_ok then
+    print('Error loading lspconfig for typescript')
+    return
+end
+
+lspconfig['lemminx'].setup({})
+
