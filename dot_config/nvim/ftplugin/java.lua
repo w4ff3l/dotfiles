f741779905b2b5687354daa3774391dbc1f13ed1
W4ff3l <w4ff3l@gmail.com>
2024-07-13 00:21:57 +0200

    java: add different runtimes

--- dot_config/nvim/ftplugin/java.lua
+++ dot_config/nvim/ftplugin/java.lua
@@ -73,9 +73,28 @@ local config = {
                 downloadSources = true,
                 downloadJavadoc = true,
             },
+            configuration = {
+                -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+                -- And search for `interface RuntimeOption`
+                -- The `name` is NOT arbitrary, but must match one of the elements from `enum ExecutionEnvironment` in the link above
+                runtimes = {
+                    {
+                        name = "JavaSE-11",
+                        path = "/usr/lib/jvm/java-11-openjdk/",
+                    },
+                    {
+                        name = "JavaSE-17",
+                        path = "/usr/lib/jvm/java-17-openjdk/",
+                    },
+                    {
+                        name = "JavaSE-21",
+                        path = "/usr/lib/jvm/java-21-openjdk/",
+                    },
+                }
+            }
         }
     },
 }
 
-        -- vim.keymap.set('n', '<space>oi', "<Cmd>lua require'jdtls'.organize_imports()<CR>")
+-- vim.keymap.set('n', '<space>oi', "<Cmd>lua require'jdtls'.organize_imports()<CR>")
 jdtls.start_or_attach(config)
