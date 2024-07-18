2e010da403b22e7560ac7dbc8e0a3de37908ccb5
W4ff3l <w4ff3l@gmail.com>
2024-07-18 07:29:33 +0200

    i3: fix move_to_next_workspace; add binding for rofi games menu

--- dot_config/i3/script/executable_move_to_next_workspace.sh
+++ dot_config/i3/script/executable_move_to_next_workspace.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-current_display_output=$(i3-msg -t get_workspaces | jq '.[] | select( .focused ) | .output')
-index_last_workspace=$(i3-msg -t get_workspaces | jq 'map( select( .output == '"${current_display_output}"' ) ) | sort_by(.id) | last | .num')
+focused_workspace=$(i3-msg -t get_workspaces | jq '.[] | select( .focused ) | .num')
 
 if [[ $1 == "next" ]]; then
-    target_workspace=$((index_last_workspace + 1))
-    echo "$target_workspace"
+    target_workspace=$((focused_workspace + 1))
 fi
 
 if [[ $1 == "previous" ]]; then
-    target_workspace=$((index_last_workspace - 1))
+    target_workspace=$((focused_workspace - 1))
 fi
 
 i3-msg move container to workspace number $target_workspace
