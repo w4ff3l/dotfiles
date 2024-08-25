e67e3d80969fe5f3f7c45f8a667b8b8134fd22ff
W4ff3l <w4ff3l@gmail.com>
2024-08-25 11:16:19 +0200

    auto-session: format config

--- dot_config/nvim/lua/user/auto-session.lua
+++ dot_config/nvim/lua/user/auto-session.lua
@@ -5,11 +5,12 @@ if not status_ok then
 end
 
 auto_session.setup {
+    auto_session_enabled = vim.env.KITTY_SCROLLBACK_NVIM ~= 'true',
     cwd_change_handling = {
-        restore_upcoming_session = true, -- Disabled by default, set to true to enable
-        pre_cwd_changed_hook = nil,    -- already the default, no need to specify like this, only here as an example
+        restore_upcoming_session = true,   -- Disabled by default, set to true to enable
+        pre_cwd_changed_hook = nil,        -- already the default, no need to specify like this, only here as an example
         post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
-            require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
+            require("lualine").refresh()   -- refresh lualine so the new session name is displayed in the status bar
         end,
     },
 }
