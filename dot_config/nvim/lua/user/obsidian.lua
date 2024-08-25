48c49daba926926f600529366711281912864c87
W4ff3l <w4ff3l@gmail.com>
2024-08-25 11:20:54 +0200

    obsidian: add passthrough

--- dot_config/nvim/lua/user/obsidian.lua
+++ dot_config/nvim/lua/user/obsidian.lua
@@ -10,17 +10,51 @@ obsidian.setup({
             name = "personal",
             path = "~/vaults/personal",
         },
---        {
---            name = "work",
---            path = "~/vaults/work",
---        },
+        {
+            name = "work",
+            path = "~/vaults/work",
+        },
     },
+    mappings = {
+        -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+        ["gd"] = {
+            action = function()
+                return require("obsidian").util.gf_passthrough()
+            end,
+            opts = { noremap = false, expr = true, buffer = true },
+        },
+        -- Toggle check-boxes.
+        ["<leader>ch"] = {
+            action = function()
+                return require("obsidian").util.toggle_checkbox()
+            end,
+            opts = { buffer = true },
+        },
+        -- Smart action depending on context, either follow link or toggle checkbox.
+        ["<cr>"] = {
+            action = function()
+                return require("obsidian").util.smart_action()
+            end,
+            opts = { buffer = true, expr = true },
+        }
+    },
+    -- Optional, customize how note IDs are generated given an optional title.
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+        -- In this case a note with the title 'My new note' will be given an ID that looks
+        -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+        local suffix = ""
+        if title ~= nil then
+            -- If title is given, transform it into valid file name.
+            suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+        else
+            -- If title is nil, just add 4 random uppercase letters to the suffix.
+            for _ = 1, 4 do
+                suffix = suffix .. string.char(math.random(65, 90))
+            end
+        end
+        return tostring(os.time()) .. "-" .. suffix
+    end,
 })
-
-vim.keymap.set("n", "gd", function()
-    if obsidian.util.cursor_on_markdown_link() then
-        return "<cmd>ObsidianFollowLink<CR>"
-    else
-        return "gd"
-    end
-end, { noremap = false, expr = true })
