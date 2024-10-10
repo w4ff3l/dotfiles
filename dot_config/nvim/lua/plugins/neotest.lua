55f6d68b984a507ad9859d0d31a30115dabf98f2
W4ff3l <w4ff3l@gmail.com>
2024-10-10 19:05:53 +0200

    neotest: fix broken setup

--- dot_config/nvim/lua/plugins/neotest.lua
+++ dot_config/nvim/lua/plugins/neotest.lua
@@ -1,16 +1,31 @@
 return {
-    'nvim-neotest/neotest',
-    dependencies = {
-        'nvim-neotest/nvim-nio',
-        'nvim-lua/plenary.nvim',
-        'antoinemadec/FixCursorHold.nvim',
-        'nvim-treesitter/nvim-treesitter'
+    {
+        'nvim-neotest/neotest',
+        dependencies = {
+            'nvim-neotest/nvim-nio',
+            'nvim-lua/plenary.nvim',
+            "antoinemadec/FixCursorHold.nvim",
+            'nvim-treesitter/nvim-treesitter',
+            'rcasia/neotest-java',
+            'rouge8/neotest-rust',
+        },
+        config = function()
+            local compatible_path = require("neotest-java.util.compatible_path")
+            require 'neotest'.setup({
+                adapters = {
+                    require 'neotest-java' ({
+                        ignore_wrapper = false,
+                        junit_jar = compatible_path(vim.fn.stdpath("data") ..
+                        "/neotest-java/junit-platform-console-standalone-1.10.0.jar"),
+                        log_level = debug,
+                    })
+                }
+            })
+            -- require 'neotest'.setup({
+            --     adapters = {
+            --         require 'neotest-rust' ({})
+            --     }
+            -- })
+        end
     },
-    opts = {
-        adapters = {
-            ['neotest-plenary'] = {},
-            ['neotest-java'] = {},
-            ['neotest-rust'] = {},
-        }
-    }
 }
