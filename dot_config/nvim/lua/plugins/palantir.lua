eab7d0407ef57f5911babf3a65e6dd8862bb18a1
W4ff3l <w4ff3l@gmail.com>
2024-10-10 19:09:18 +0200

    palantir: init custom plugin

---
+++ dot_config/nvim/lua/plugins/palantir.lua
@@ -0,0 +1,9 @@
+return {
+    dir = '~/workspace/lua/palantir.nvim',
+    name = 'palantir',
+    event = 'VeryLazy',
+    dependencies = {
+        'rcarriga/nvim-notify',
+    },
+    opts = {},
+}
