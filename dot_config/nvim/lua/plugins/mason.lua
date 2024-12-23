e0a210c910a9237b3b6a549a25fa0f640d4791e5
w4ff3l <w4ff3l@gmail.com>
2024-12-23 16:02:53 +0100

    mason: add lspconfig

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -1,19 +1,30 @@
 return {
     {
-    'williamboman/mason.nvim',
-    cmd = 'Mason',
-    config = function ()
-        require'mason'.setup()
-    end
+        'williamboman/mason.nvim',
+        cmd = 'Mason',
+        config = function()
+            require 'mason'.setup()
+        end
     },
     {
-    'williamboman/mason-lspconfig.nvim',
-    event = { 'BufReadPre', 'BufNewFile' },
-    dependencies = {
-      'williamboman/mason.nvim'
-    },
-    config = function ()
-        require'mason-lspconfig'.setup()
-    end
+        'williamboman/mason-lspconfig.nvim',
+        event = { 'BufReadPre', 'BufNewFile' },
+        dependencies = {
+            'williamboman/mason.nvim'
+        },
+        config = function()
+            require 'mason-lspconfig'.setup({
+                ensure_installed = {
+                    "jsonls",
+                    "gradle_ls",
+                    "angularls",
+                    "bashls",
+                    "cssls",
+                    "sqlls",
+                    "lua_ls",
+                    "jdtls",
+                }
+            })
+        end
     }
 }
